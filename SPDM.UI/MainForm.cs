5620234e991d40052be43acd58c93846b0115f5e
khandokar <khandokarsabbir@gmail.com>
2024-06-23 16:14:39 -0700

    asasd

--- SPDM.UI/MainForm.cs
+++ SPDM.UI/MainForm.cs
@@ -1,13 +1,5 @@
-﻿using SPDM.BLL.BusinessLogic;
-using SPDM.UI.Reports;
+﻿using SPDM.UI.Reports;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SPDM.UI
