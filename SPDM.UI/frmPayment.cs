d7c856b5b6308b58c8ff9d85a15f4a1f58cc5046
khandokar <khandokarsabbir@gmail.com>
2025-07-06 14:48:32 -0700

    Clean frmPayment

--- SPDM.UI/frmPayment.cs
+++ SPDM.UI/frmPayment.cs
@@ -1,13 +1,6 @@
 ﻿using SPDM.BLL.BusinessLogic;
 using SPDM.DLL.Entities;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SPDM.UI
